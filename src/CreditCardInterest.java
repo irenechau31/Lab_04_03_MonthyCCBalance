09c2bc4af7c341ddd208a740514d33a642cbf135
irenechau31 <beasthoang01@gmail.com>
2023-07-03 11:18:42 +0700

    changing computation

--- src/CreditCardInterest.java
+++ src/CreditCardInterest.java
@@ -1,10 +1,9 @@
 public class CreditCardInterest {
     public static void main(String[] args) {
-        System.out.println("What is the credit card balance?");
-        double creditCardBalance = 0;
+        double creditCardBalance = 5000;
         double interestRate = 0.17;
-        double interestOneMonth = creditCardBalance * interestRate;
-        double interestTwoMonths = (creditCardBalance + interestOneMonth) * interestRate;
+        double interestOneMonth = creditCardBalance * (interestRate + 1);
+        double interestTwoMonths = interestOneMonth * (interestRate + 1);
         System.out.println("Interest due after one month is: " + interestOneMonth );
         System.out.println("Interest due after two months is: " + interestTwoMonths );
     }
